2115d525b052daf0c61ee294ee76282f9e7fd0d2
Plesca <aplesca@techteam.com>
2020-07-14 14:35:32 +0300

    task 11 correction

--- fundamentals/task11/logical.js
+++ fundamentals/task11/logical.js
@@ -1,29 +1,38 @@
-// alert( null || 2 || undefined );    // 2
+// ex 1
+    alert( null || 2 || undefined );    // 2
 
-// alert( alert(1) || 2 || alert(3) ); // 1 2
+// ex 2
+    alert( alert(1) || 2 || alert(3) ); // 1 2
 
-// alert( 1 && null && 2 );            // null
+// ex 3
+    alert( 1 && null && 2 );            // null
 
-// alert( alert(1) && alert(2) );      // 1 undefined
+// ex 4
+    alert( alert(1) && alert(2) );      // 1 undefined
 
-// alert( null || 2 && 3 || 4 );       // 3
+// ex 5
+    alert( null || 2 && 3 || 4 );       // 3
 
-// let age;
-// if (age >= 14 && age <= 90);
+// ex 6
+    let age;
+    if (age >= 14 && age <= 90);
 
-// if (age < 14 || age > 90);
-// if (!(age >= 14 && age <= 90));
+// ex 7
+    if (age < 14 || age > 90);
+    if (!(age >= 14 && age <= 90));
 
-// if (-1 || 0) alert( 'first' );          // first
-// if (-1 && 0) alert( 'second' );         // nothing
-// if (null || -1 && 1) alert( 'third' );  // third
+// ex 8
+    if (-1 || 0) alert( 'first' );          // first
+    if (-1 && 0) alert( 'second' );         // nothing
+    if (null || -1 && 1) alert( 'third' );  // third
 
-let name = prompt("Who's there?");
-if      (!name)     alert("Canceled")
-else if (name != "Admin")   alert("I don't know you")
-else {
-    let password = prompt("Password");
-    if      (!password)         alert("Canceled")
-    else if (password != "TheMaster")   alert("Wrong password")
-    else alert("Welcome")
-}
+// ex 9
+    let name = prompt("Who's there?");
+    if      (!name)     alert("Canceled")
+    else if (name != "Admin")   alert("I don't know you")
+    else {
+        let password = prompt("Password");
+        if      (!password)         alert("Canceled")
+        else if (password != "TheMaster")   alert("Wrong password")
+        else alert("Welcome")
+    }
